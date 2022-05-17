bf9fec7a912c39f639735dd82809297e5f826a8e
rknob <rknob@inpulse.vet>
2022-05-17 11:51:35 -0300

    fiz exercicio1

--- first week/exercicio 1.py
+++ first week/exercicio 1.py
@@ -1,1 +1,25 @@
-print("Hello World")
+import random
+import turtle as t
+from unittest import result
+
+t.pensize(2)
+t.speed(1)
+
+def bolsa():
+    x = 0
+    for _ in range(200):
+        y = random.randint(-200, 200)
+        if y >= 0:
+            t.color("green")
+            t.goto(x, y)
+        else:
+            t.color("red")
+            t.goto(x, y)
+        x = x + 20
+        z = y + y
+    return print(z)
+
+
+bolsa()
+
+
