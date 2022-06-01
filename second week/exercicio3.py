0c03e6fa9b1e6c95029d3579bf68748227e71d24
eduardo pavei <eduardopavei123@gmail.com>
2022-06-01 15:09:43 -0300

    exercicio 3

---
+++ second week/exercicio3.py
@@ -0,0 +1,17 @@
+def number(y):
+    sum = 0
+    num_str = str(y)
+    for x in range(len(num_str)):
+        sum += int(num_str[x]) ** (x+1)
+    if sum == y:
+        print(sum, "---->", True)
+
+
+def verification():
+    for i in range(1000000000):
+        number(i)
+
+print("fim")
+
+
+verification()
