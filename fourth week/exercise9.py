7f0323ef01d79e99fc1faa4dd44ee16106b2141a
eduardo pavei <eduardopavei123@gmail.com>
2022-06-28 11:41:58 -0300

    exercicio 10

--- fourth week/exercise9.py
+++ fourth week/exercise9.py
@@ -1,6 +1,8 @@
 #Usando o while, implemente uma função que inverte uma lista dada como argumento,
 # ou seja, o último elemento vira o primeiro da lista e vice-versa ([1,2,3] vira [3,2,1]).
 # Não use lista[::-1], nem reverse().
+import unittest
+
 
 def inverte_list(number_list):
     i = 0
@@ -13,5 +15,18 @@ def inverte_list(number_list):
     return number_list
 
 
-lista = list(range(10))
-print(inverte_list(lista))
+
+class MyTest(unittest.TestCase):
+
+    def test_empty_case(self):
+        inverted = inverte_list([])
+        print(inverted)
+        self.assertIs(inverted, [])
+
+    def test_normal_case(self):
+        original = list(range(4))
+        inverted = inverte_list(original)
+        self.assertIs(original, list(range(4)))
+        self.assertIs(inverted, original.reverse())
+
+
