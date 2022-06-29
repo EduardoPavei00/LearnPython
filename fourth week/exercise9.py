a449c11621591b216daf1530c1b32fa8dd295e87
eduardo pavei <eduardopavei123@gmail.com>
2022-06-29 11:04:10 -0300

    exercicio 10

--- fourth week/exercise9.py
+++ fourth week/exercise9.py
@@ -6,14 +6,13 @@ import unittest
 
 def inverte_list(number_list):
     i = 0
+    new_list = []
     tamanho_string = len(number_list)
     while i < tamanho_string:
-        number_list.append(tamanho_string - i)
-        number_list.pop(0)
+        new_list.append((tamanho_string - i)-(1))
         i = i + 1
 
-    return number_list
-
+    return new_list
 
 
 class MyTest(unittest.TestCase):
@@ -21,12 +20,16 @@ class MyTest(unittest.TestCase):
     def test_empty_case(self):
         inverted = inverte_list([])
         print(inverted)
-        self.assertIs(inverted, [])
+        self.assertEqual(inverted, [])
 
     def test_normal_case(self):
         original = list(range(4))
-        inverted = inverte_list(original)
-        self.assertIs(original, list(range(4)))
-        self.assertIs(inverted, original.reverse())
+        inverted = inverte_list(list(range(4)))
+        self.assertEqual(original,  list(range(4)))
+        original.reverse()
+        self.assertEqual(inverted,  original)
+
+
+
 
 
